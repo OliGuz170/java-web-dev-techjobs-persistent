eed2890592ae9bf3fe9bbedb4c6bd67339e281d9
Olivia Guzman <olguva@gmail.com>
2021-03-22 22:29:57 -0500

    Completed up to Part 2 Data Layer(1-2)

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String location;
 
+    public Employer(String location){
+        this.location = location;
+    }
+
+    //added a no-arg constructor to instantiate an object
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
